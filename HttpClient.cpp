f8b1e763925a8d2ec2134983a542bc6a5d950391
pcan942 <vincent@mireau.com>
2014-09-11 15:38:41 +0200

    améliorations / version non stable

--- HttpClient.cpp
+++ HttpClient.cpp
@@ -2,8 +2,24 @@
 #include "HttpClient.h"
 
 HttpClient::HttpClient(Client& cl) : client(&cl){
-	http_status = HTTP_STATUS_IDLE;
-	http_timeout = HTTP_DEFAULT_TIMEOUT;
+	status = HTTP_STATUS_IDLE;
+	timeout = HTTP_DEFAULT_TIMEOUT;
+	debug = false;
+}
+
+void HttpClient::setTimeout(int millis){
+	if(millis>0) timeout = millis;
+}
+
+void HttpClient::reset(){
+	if(client->connected()){
+		client->stop();
+	}
+	status = HTTP_STATUS_IDLE;
+}
+
+void HttpClient::setDebug(boolean d){
+	debug = d;
 }
 
 /**
@@ -12,34 +28,62 @@ HttpClient::HttpClient(Client& cl) : client(&cl){
  *    HTTP_ERROR_CONNECTION_FAILED si la conexion a échoué
  */
 int HttpClient::sendGet(char* server, int port, char* path){
-	if(http_status!=HTTP_STATUS_IDLE) return HTTP_ERROR;
+	if(status!=HTTP_STATUS_IDLE) return HTTP_ERROR;
 	
-	if( !client.connect(server, port) ){
-		//KO
-		client.stop();
-		return HTTP_ERROR_CONNECTION_FAILED;
-	}
+	if( connect(server, port) ) return HTTP_ERROR_CONNECTION_FAILED;
 	
-	client.print("GET ");
-	client.print(path);
-    client.print(" HTTP/1.1\nHost: ");
-	client.println(server);
-	client.println("Connection: close\n");
+	print("GET ");
+	print(path);
+    print(" HTTP/1.1\nHost: ");
+	println(server);
+	println("Connection: close\n");
+}
+
+int HttpClient::print(char* str){
+	client->print(str);
+	if(debug) Serial.print(str);
+}
+int HttpClient::println(char* str){
+	client->println(str);
+	if(debug) Serial.println(str);
 }
 
 /**
- * Lecture de la première ligne de la réponse, avec le code réponse
- *	@return int code réponse, ou HTTP_RESP_ERROR s'il y a eu une erreur
+ * Connection au serveur
+ */
+boolean HttpClient::connect(char* serv, int port){
+	if( !client->connect(serv, port) ){
+		//KO
+		client->stop();
+		if( !client->connect(serv, port) ){
+			Serial.println(F("HTTP connection failed. try to force client.stop()"));
+
+			//On essaye simplement de forcer la fermeture de la socket
+			client->stop();
+			delay(500); //Don't know if necessary
+			if( !client->connect(serv, port) ) {
+			//Ca n'a pas marché
+			return false;
+			}
+		}
+	}
+	return true;
+}
+
+/**
+ * Attend le début de la réponse, lit la première ligne de la réponse, avec le code réponse
+ * Fait suite à l'envoi d'une requête.
+ *	@return int code réponse
  *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_RESP_EOF si la fin de fichier a été rencontrée
- *		 HTTP_RESP_END_OF_HEARDERS s'il n'y a plus d'en-têtes
+ *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
-int HttpClient::http_resp_read_code(){
-	if(http_status>HTTP_STATUS_RESP_BEGIN) return HTTP_ERROR;
+int HttpClient::readResponseCode(){
+	if(status!=HTTP_STATUS_RESP_WAIT) return HTTP_ERROR;
+	
 	char* intro = "HTTP/1.1 ";
 	int result = 0;
-	http_start_millis = millis();
+	startMillis = millis();
 	
 	for(int charIdx=0;;charIdx++){
 		int c = httpRead();
@@ -60,8 +104,8 @@ int HttpClient::http_resp_read_code(){
 		}
 		else{
 			//On passe le reste de la ligne, et on renvoie le résultat
-			if(int res=httpRespMoveToEOL()<0) return res;
-			http_status = HTTP_STATUS_RESP_HEADER;	//La ligne suivante est une en-tête
+			if(int res=moveToEOL()<0) return res;
+			status = HTTP_STATUS_RESP_HEADER;	//La ligne suivante est une en-tête
 			return result;
 		}
 	}
@@ -70,13 +114,13 @@ int HttpClient::http_resp_read_code(){
 /**
  * Ligne d'entête HTTP : lecture de la clé uniquement
  *	@return int nb de caractères lus
+ *       0 signifie qu'il n'y a plus d'en-tête : on passe au body
  *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_RESP_EOF si la fin de fichier a été rencontrée
- *		 HTTP_RESP_END_OF_HEARDERS s'il n'y a plus d'en-têtes
+ *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
-int HttpClient::http_resp_read_next_header_key(char* key,int maxlen){
-	if(http_status!=HTTP_STATUS_RESP_HEADER){
+int HttpClient::readNextHeaderKey(char* key,int maxlen){
+	if(status!=HTTP_STATUS_RESP_HEADER){
 		//On n'est pas en début de ligne d'en-tête
 		return HTTP_ERROR;
 	}
@@ -88,17 +132,17 @@ int HttpClient::http_resp_read_next_header_key(char* key,int maxlen){
 		if(c=='\r') continue;	//on ignore le '\r'
 		if(c=='\n' && nbChars==0){
 			//C'est la fin des en-têtes
-			http_status = HTTP_STATUS_RESP_BODY;
-			return HTTP_RESP_END_OF_HEARDERS;
+			status = HTTP_STATUS_RESP_BODY;
+			return 0;
 		}
 		if(c==':'){
 			//Fin de la clé
-			http_status = HTTP_STATUS_RESP_HEADER_VALUE; //L'étape suivante est de lire la valeur
+			status = HTTP_STATUS_RESP_HEADER_VALUE; //L'étape suivante est de lire la valeur
 			return nbChars;
 		}
 		if(c>='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9' || c=='_' || c=='-'){
 			nbChars++;
-			if(nbChars>maxlen) return HTTP_ERROR;
+			if(nbChars>maxlen) return HTTP_ERROR_BUFFER_OVERFLOW;
 			key[nbChars-1] = c;
 			key[nbChars] = '\0';
 		}
@@ -109,8 +153,16 @@ int HttpClient::http_resp_read_next_header_key(char* key,int maxlen){
 	}
 }
 
-int HttpClient::http_resp_read_next_header_value(char* value,int maxlen){
-	if(http_status!=HTTP_STATUS_RESP_HEADER_VALUE){
+/**
+ * Ligne d'entête HTTP : lecture de la valeur
+ * Doit faire suite à readNextHeaderKey
+ *	@return int nb de caractères lus
+ *		 HTTP_ERROR s'il y a eu une erreur
+ *		 HTTP_EOF si la fin de fichier a été rencontrée
+ *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
+ */
+int HttpClient::readNextHeaderValue(char* value,int maxlen){
+	if(status!=HTTP_STATUS_RESP_HEADER_VALUE){
 		//Le curseur n'est pas au début d'une valeur d'en-tête
 		return HTTP_ERROR;
 	}
@@ -122,7 +174,7 @@ int HttpClient::http_resp_read_next_header_value(char* value,int maxlen){
 		if(c=='\r') continue;	//on ignore le '\r'
 		if(c=='\n'){
 			//C'est la fin de la valeur
-			http_status = HTTP_STATUS_RESP_HEADER;
+			status = HTTP_STATUS_RESP_HEADER;
 			return nbChars;
 		}
 		if(nbChars>=maxlen){
@@ -135,8 +187,33 @@ int HttpClient::http_resp_read_next_header_value(char* value,int maxlen){
 	}
 }
 
-int HttpClient::httpRespMoveToEOL(){
-	if(http_status<HTTP_STATUS_RESP_BEGIN){
+/**
+ * Passe la suites des en-têtes HTTP
+ *	@return 0 si tout s'est bien passé
+ *		 HTTP_ERROR s'il y a eu une erreur
+ *		 HTTP_EOF si la fin de fichier a été rencontrée
+ *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
+ */
+int HttpClient::skipHeaders(){
+	if(status!=HTTP_STATUS_RESP_HEADER && status!=HTTP_STATUS_RESP_HEADER_VALUE){
+		//On n'est pas dans les en-têtes
+		return HTTP_ERROR;
+	}
+	
+	int prevChar;
+	int c = 0;
+	do{
+		prevChar = c;
+		int c = httpRead();
+		if(c<0) return c;	//httpRead a renvoyé une erreur
+		if(c=='\r') continue;
+	}
+	while(c!='\n' || c!=prevChar); //tant qu'il n'y a pas deux caractères '\n' consécutifs
+	return 0;
+}
+
+int HttpClient::moveToEOL(){
+	if(status==HTTP_STATUS_IDLE){
 		//La réponse n'a pas commencé
 		return HTTP_ERROR;
 	}
@@ -144,7 +221,7 @@ int HttpClient::httpRespMoveToEOL(){
 		int c = httpRead();
 		if(c<0) return c;	//httpRead a renvoyé une erreur
 		if(c=='\n'){
-			if(http_status<=HTTP_STATUS_RESP_HEADER_VALUE) http_status = HTTP_STATUS_RESP_HEADER;
+			if(status<=HTTP_STATUS_RESP_HEADER_VALUE) status = HTTP_STATUS_RESP_HEADER;
 			//sinon on est dans le corps : on ne change pas le status
 			return 0;
 		}
@@ -152,40 +229,35 @@ int HttpClient::httpRespMoveToEOL(){
 }
 
 /**
- * Attend des données de réponse http
- * @return 0 si tout va bien
- *		 HTTP_RESP_EOF si la fin de fichier a été rencontrée
- *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
+ * Indique si des données de réponse sont arrivées
  */
-int HttpClient::httpWaitForResponse(){
-	while(!client->available()){
-		//on attend tant qu'il n'y a rien à lire, qu'on n'est pas déconnecté, et que le timeout n'est pas dépassé
-		if (client->connected() == 0){
-			http_status = HTTP_STATUS_EOF;
-			return HTTP_RESP_EOF;
-		}
-		if(millis()-http_start_millis > http_timeout){
-			http_status = HTTP_STATUS_ERROR;
-			return HTTP_ERROR_TIMEOUT;	 
-		}
-	}
-	return 0;
+int HttpClient::available(){
+	return client->available();
 }
 
 /**
  * Renvoie le caractère suivant de la réponse, en attendant si nécessaire
  * @return caractère suivant dans la réponse
- *		 HTTP_RESP_EOF si la fin de fichier a été rencontrée
+ *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
 int HttpClient::httpRead(){
-	if(int res=httpWaitForResponse() != 0) return res;
+	//on attend tant qu'il n'y a rien à lire, qu'on n'est pas déconnecté, et que le timeout n'est pas dépassé
+	while(!client->available()){
+		if ( !client->connected() ){
+			status = HTTP_STATUS_EOF;
+			return HTTP_EOF;
+		}
+		if(millis()-startMillis > timeout){
+			status = HTTP_STATUS_ERROR;
+			return HTTP_ERROR_TIMEOUT;	 
+		}
+	}
 	
-	//On ignore le caractère '\r'
 	int c = client->read();
 	if(c==EOF){
-		http_status = HTTP_STATUS_EOF;
-		return HTTP_RESP_EOF;
+		status = HTTP_STATUS_EOF;
+		return HTTP_EOF;
 	}
 	return c;
 }
--- HttpClient.h
+++ HttpClient.h
@@ -10,40 +10,54 @@ public:
 	HttpClient(Client& cl);
 	
 	//Les status que peut prendre la connection HTTP
-	#define HTTP_STATUS_IDLE 0              //Aucune requete http en cours
-	#define HTTP_STATUS_RESP_WAIT 1         //En attente de la réponse
-	#define HTTP_STATUS_RESP_BEGIN 2        //Au début de la réponse (avant HTTP/1.1)
-	#define HTTP_STATUS_RESP_HEADER 3       //Au début d'une ligne d'en-tête
-	#define HTTP_STATUS_RESP_HEADER_VALUE 4 //Ligne d'en-tête, après le ':' (clé déjà lue)
-	#define HTTP_STATUS_RESP_BODY 5         //corps de la réponse
-	#define HTTP_STATUS_ERROR 6             //Une erreur est survenue
-	#define HTTP_STATUS_EOF 7               //La fin de fichier a été atteinte
+	typedef enum {
+	    HTTP_STATUS_IDLE,                //Aucune requete http en cours
+	    HTTP_STATUS_RESP_WAIT,           //En attente de la réponse
+	    HTTP_STATUS_RESP_BEGIN,          //Au début de la réponse (avant HTTP/1.1)
+	    HTTP_STATUS_RESP_HEADER,         //Au début d'une ligne d'en-tête
+	    HTTP_STATUS_RESP_HEADER_VALUE,   //Ligne d'en-tête, après le ':' (clé déjà lue)
+	    HTTP_STATUS_RESP_BODY,           //Corps de la réponse
+	    HTTP_STATUS_ERROR,               //Une erreur est survenue
+		HTTP_STATUS_EOF                  //La fin de fichier a été atteinte
+	} HttpState;
 	
 	//Les codes retours possibles (négatifs pour les différencier du nombre de caractères que peuvent renvoyer certaines fonctions)
-	#define HTTP_ERROR -1					
-	#define HTTP_RESP_END_OF_HEARDERS -2    //Fin des en-têtes
-	#define HTTP_RESP_EOF -3                //Fin de réponse
+	#define HTTP_EOF -1
+	#define HTTP_ERROR -2					
+	#define HTTP_ERROR_BUFFER_OVERFLOW -3
 	#define HTTP_ERROR_TIMEOUT -4;
 	#define HTTP_ERROR_CONNECTION_FAILED -5
 	
 	#define HTTP_DEFAULT_TIMEOUT 3000;		//Timeout http par défaut
 	
+	HttpState status;                  //Status courant de la connection
+	
+	void reset();
+	void setTimeout(int millis);
+	void setDebug(boolean debug);
+	
 	int sendGet(char* server, int port, char* path);
 	
-	int http_resp_read_code();
-	int http_resp_read_next_header_key(char* key,int maxlen);
-	int http_resp_read_next_header_value(char* value,int maxlen);
-	int httpRespMoveToEOL();
-	int httpWaitForResponse();
+	int readResponseCode();
+	int readNextHeaderKey(char* key,int maxlen);
+	int readNextHeaderValue(char* value,int maxlen);
+	int skipHeaders();
+	int moveToEOL();
+	int available();
+	int waitForResponse();
 	int httpRead();
 
 protected:
 	Client* client;					//Interface Client (ethernet, GSM, Wifi ...)
 
+	int print(char* str);
+	int println(char* str);
+	boolean connect(char* serv, int port);
+	
 private:
-	int http_status;                   //Status courant de la connection
-	int http_start_millis;                  //timestamp de démarrage de la requete en cours (sert pour le timeout)
-	int http_timeout;                       //configuration de timeout en cours
+	int startMillis;                  //timestamp de démarrage de la requete en cours (sert pour le timeout)
+	int timeout;                       //configuration de timeout en cours
+	boolean debug;
 	
 };
 
