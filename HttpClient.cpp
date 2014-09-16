6c9d0501eb1aa79fcee03d144e63bfa06d6e8520
vmire <vincent_257@mireau.com>
2014-09-17 00:41:44 +0200

    bug fix

--- HttpClient.cpp
+++ HttpClient.cpp
@@ -12,61 +12,87 @@ void HttpClient::setTimeout(int millis){
 }
 
 void HttpClient::reset(){
-	if(client->connected()){
+	close();
+}
+
+void HttpClient::close(){
+	if(debug) Serial.println(F("HttpClient.close()"));
+	//if(client->connected()){
 		client->stop();
-	}
+	//}
 	status = HTTP_STATUS_IDLE;
 }
 
 void HttpClient::setDebug(boolean d){
 	debug = d;
+	Serial.print("debug=");Serial.println(debug);
 }
 
 /**
  * Envoi d'une requete GET
  * @return 0 si la connexion a réussi
- *    HTTP_ERROR_CONNECTION_FAILED si la conexion a échoué
+ *	HTTP_ERROR_CONNECTION_FAILED si la conexion a échoué
  */
-int HttpClient::sendGet(char* server, int port, char* path){
-	if(status!=HTTP_STATUS_IDLE) return HTTP_ERROR;
+int HttpClient::sendGet(char* server, int port, char* path, char* params){
+	if(debug) Serial.print(F("HttpClient.sendGet "));
 	
-	if( connect(server, port) ) return HTTP_ERROR_CONNECTION_FAILED;
-	
+	if( !connect(server, port) ){
+		return error(HTTP_ERROR_CONNECTION_FAILED);
+	}
 	print("GET ");
 	print(path);
-    print(" HTTP/1.1\nHost: ");
+	if(debug) Serial.print(path);
+	if(params[0]!='\0'){
+		print("?");
+		print(params);
+		if(debug){
+			Serial.print("?");
+			Serial.print(params);
+		}
+	}
+	print(" HTTP/1.1\nHost: ");
 	println(server);
 	println("Connection: close\n");
+	if(debug) Serial.print("\n");
+	status = HTTP_STATUS_RESP_WAIT;
+	return 0;
 }
 
 int HttpClient::print(char* str){
+	//if(debug) Serial.print(str);
 	client->print(str);
-	if(debug) Serial.print(str);
 }
 int HttpClient::println(char* str){
+	//if(debug) Serial.println(str);
 	client->println(str);
-	if(debug) Serial.println(str);
 }
 
 /**
  * Connection au serveur
  */
 boolean HttpClient::connect(char* serv, int port){
+	if(debug){
+		Serial.print(F("HttpClient.connect to "));
+		Serial.print(serv);
+		Serial.print("...");
+	}
+	if(status != HTTP_STATUS_IDLE){
+		Serial.println(F("Connection deja en cours..."));
+		return false;
+	}
+
 	if( !client->connect(serv, port) ){
+		Serial.print(F("FAILED..."));
 		//KO
 		client->stop();
+		delay(500); //Don't know if necessary
 		if( !client->connect(serv, port) ){
-			Serial.println(F("HTTP connection failed. try to force client.stop()"));
-
-			//On essaye simplement de forcer la fermeture de la socket
-			client->stop();
-			delay(500); //Don't know if necessary
-			if( !client->connect(serv, port) ) {
-			//Ca n'a pas marché
+			Serial.println(F("FAILED"));
 			return false;
-			}
 		}
 	}
+	if(debug) Serial.println("OK");
+	lastConnected = true;
 	return true;
 }
 
@@ -75,115 +101,83 @@ boolean HttpClient::connect(char* serv, int port){
  * Fait suite à l'envoi d'une requête.
  *	@return int code réponse
  *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
 int HttpClient::readResponseCode(){
-	if(status!=HTTP_STATUS_RESP_WAIT) return HTTP_ERROR;
-	
+	if(debug) Serial.print(F("HttpClient.readResponseCode..."));
+	if(status!=HTTP_STATUS_RESP_WAIT){
+		if(debug) Serial.println(F("Excepted status:WAIT"));
+		return error(HTTP_ERROR_STATUS);
+	}
 	char* intro = "HTTP/1.1 ";
 	int result = 0;
 	startMillis = millis();
 	
 	for(int charIdx=0;;charIdx++){
-		int c = httpRead();
+		char c = httpRead();
+
 		if(c<0) return c;	//C'est une erreur
 		
 		if(charIdx<=8){
 			//Vérification du "HTTP/1.1 "
 			if(c!=intro[charIdx]){
-				return HTTP_ERROR;
+				return error(HTTP_ERROR);
 			}
 		}
 		else if(charIdx<=11){
 			//Lecture du code réponse
 			if(c<'0' || c >'9'){
-				return HTTP_ERROR;
+				return error(HTTP_ERROR);
 			}
-			result += (c-'0')*10^(11-charIdx);
+			int tmp = 1;
+			for(uint8_t i=0;i<11-charIdx;i++) tmp = tmp*10;
+			tmp = (c-48)*tmp;
+			result += tmp;
+			//Serial.print("c=");Serial.print(c-48,DEC);Serial.print(" ");Serial.print(tmp);Serial.print("   idx=");Serial.print(charIdx,DEC);Serial.print("  ->");Serial.println(result,DEC);
+			//if(debug) Serial.println(result);
 		}
 		else{
 			//On passe le reste de la ligne, et on renvoie le résultat
-			if(int res=moveToEOL()<0) return res;
+			if(int res=moveToEOL()<0) return error(res);
 			status = HTTP_STATUS_RESP_HEADER;	//La ligne suivante est une en-tête
+			if(debug) Serial.println(result);
 			return result;
 		}
 	}
 }
 
 /**
- * Ligne d'entête HTTP : lecture de la clé uniquement
- *	@return int nb de caractères lus
- *       0 signifie qu'il n'y a plus d'en-tête : on passe au body
+ * Lecture de la réponse jusqu'à un certain caractère (exclu) ou la fin de ligne
+ * Le caractère '\r' est ignoré
+ *	@return int nb de caractères lus (0 signifie que le premier caractère était ce caractère ou une fin de ligne)
  *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
-int HttpClient::readNextHeaderKey(char* key,int maxlen){
-	if(status!=HTTP_STATUS_RESP_HEADER){
-		//On n'est pas en début de ligne d'en-tête
-		return HTTP_ERROR;
-	}
-	
+int HttpClient::readLineToChar(char sep, char* key,int maxlen){
+	if(debug) Serial.println(F("HttpClient.readLineToChar"));
 	for(int nbChars=0;;){
-		int c = httpRead();
+		char c = httpRead();
+
+		if(c==sep || c==0) return nbChars;	//Séparateur ou fin de fichier
 		if(c<0) return c;	//httpRead a renvoyé une erreur
-		
-		if(c=='\r') continue;	//on ignore le '\r'
-		if(c=='\n' && nbChars==0){
+		if(c==10) continue;	//on ignore le '\r'
+		if(c==13 && nbChars==0 && status!=HTTP_STATUS_RESP_BODY){
 			//C'est la fin des en-têtes
 			status = HTTP_STATUS_RESP_BODY;
 			return 0;
 		}
-		if(c==':'){
-			//Fin de la clé
-			status = HTTP_STATUS_RESP_HEADER_VALUE; //L'étape suivante est de lire la valeur
-			return nbChars;
-		}
-		if(c>='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9' || c=='_' || c=='-'){
-			nbChars++;
-			if(nbChars>maxlen) return HTTP_ERROR_BUFFER_OVERFLOW;
-			key[nbChars-1] = c;
-			key[nbChars] = '\0';
-		}
-		else{
+		if(status==HTTP_STATUS_RESP_HEADER && !(c>='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9' || c=='_' || c=='-')){
 			//Caractère invalide
-			return HTTP_ERROR;
+			if(debug){
+				Serial.print(F("Caractere invalide:")); Serial.print(c); Serial.println(c,DEC);
+			}
+			return error(HTTP_ERROR);
 		}
-	}
-}
 
-/**
- * Ligne d'entête HTTP : lecture de la valeur
- * Doit faire suite à readNextHeaderKey
- *	@return int nb de caractères lus
- *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_EOF si la fin de fichier a été rencontrée
- *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
- */
-int HttpClient::readNextHeaderValue(char* value,int maxlen){
-	if(status!=HTTP_STATUS_RESP_HEADER_VALUE){
-		//Le curseur n'est pas au début d'une valeur d'en-tête
-		return HTTP_ERROR;
-	}
-	value[0] = '\0';
-	for(int nbChars=0;;){
-		int c = httpRead();
-		if(c<0) return c;	//httpRead a renvoyé une erreur
-		
-		if(c=='\r') continue;	//on ignore le '\r'
-		if(c=='\n'){
-			//C'est la fin de la valeur
-			status = HTTP_STATUS_RESP_HEADER;
-			return nbChars;
-		}
-		if(nbChars>=maxlen){
-			//Longueur de chaine dépassée
-			return HTTP_ERROR;
-		}
-		
-		value[nbChars++] = c;
-		value[nbChars+1] = '\0';
+		nbChars++;
+		if(nbChars>maxlen) return error(HTTP_ERROR_BUFFER_OVERFLOW);
+		key[nbChars-1] = c;
+		key[nbChars] = '\0';
 	}
 }
 
@@ -191,38 +185,44 @@ int HttpClient::readNextHeaderValue(char* value,int maxlen){
  * Passe la suites des en-têtes HTTP
  *	@return 0 si tout s'est bien passé
  *		 HTTP_ERROR s'il y a eu une erreur
- *		 HTTP_EOF si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
 int HttpClient::skipHeaders(){
-	if(status!=HTTP_STATUS_RESP_HEADER && status!=HTTP_STATUS_RESP_HEADER_VALUE){
+	if(debug) Serial.println(F("HttpClient.skipHeaders"));
+	if(status!=HTTP_STATUS_RESP_HEADER){
 		//On n'est pas dans les en-têtes
-		return HTTP_ERROR;
+		if(debug) Serial.println("Excepted status:RESP_HEADER");
+		return error(HTTP_ERROR_STATUS);
 	}
 	
-	int prevChar;
-	int c = 0;
-	do{
+	char prevChar;
+	char c = 0;
+	while(c!=13 || prevChar!=13) //tant qu'il n'y a pas deux caractères '\n' consécutifs
+	{
 		prevChar = c;
-		int c = httpRead();
-		if(c<0) return c;	//httpRead a renvoyé une erreur
-		if(c=='\r') continue;
+		c = httpRead();
+		if(c<0) return error(c);	//httpRead a renvoyé une erreur
+		if(c==10){
+			c = prevChar;	
+			prevChar = 0;
+			continue;
+		}
 	}
-	while(c!='\n' || c!=prevChar); //tant qu'il n'y a pas deux caractères '\n' consécutifs
+	status = HTTP_STATUS_RESP_BODY;
 	return 0;
 }
 
 int HttpClient::moveToEOL(){
+	if(debug) Serial.println(F("HttpClient.moveToEOL"));
 	if(status==HTTP_STATUS_IDLE){
 		//La réponse n'a pas commencé
-		return HTTP_ERROR;
+		if(debug) Serial.println("Error: Status not IDLE");
+		return error(HTTP_ERROR_STATUS);
 	}
 	for(int nbChars=0;;nbChars++){
 		int c = httpRead();
 		if(c<0) return c;	//httpRead a renvoyé une erreur
 		if(c=='\n'){
-			if(status<=HTTP_STATUS_RESP_HEADER_VALUE) status = HTTP_STATUS_RESP_HEADER;
-			//sinon on est dans le corps : on ne change pas le status
 			return 0;
 		}
 	}
@@ -232,13 +232,13 @@ int HttpClient::moveToEOL(){
  * Indique si des données de réponse sont arrivées
  */
 int HttpClient::available(){
-	return client->available();
+	return status!= HTTP_STATUS_ERROR && client->available();
 }
 
 /**
  * Renvoie le caractère suivant de la réponse, en attendant si nécessaire
  * @return caractère suivant dans la réponse
- *		 HTTP_EOF si la fin de fichier a été rencontrée
+ *		 O si la fin de fichier a été rencontrée
  *		 HTTP_ERROR_TIMEOUT si le timeout a été dépassé
  */
 int HttpClient::httpRead(){
@@ -246,19 +246,67 @@ int HttpClient::httpRead(){
 	while(!client->available()){
 		if ( !client->connected() ){
 			status = HTTP_STATUS_EOF;
-			return HTTP_EOF;
+			return 0;
 		}
 		if(millis()-startMillis > timeout){
 			status = HTTP_STATUS_ERROR;
-			return HTTP_ERROR_TIMEOUT;	 
+			return error(HTTP_ERROR_TIMEOUT);
 		}
 	}
 	
-	int c = client->read();
+	char c = client->read();
 	if(c==EOF){
 		status = HTTP_STATUS_EOF;
-		return HTTP_EOF;
+		return 0;
 	}
 	return c;
 }
 
+/**
+ * Ecrit un message d'erreur et renvoi le code en retour
+ */
+int HttpClient::error(int err){
+	Serial.print(F("HttpClient ERROR: "));
+	switch(err){
+		case HTTP_ERROR_BUFFER_OVERFLOW:
+			Serial.print(F("Buffer overflow"));
+			break;
+		case HTTP_ERROR_TIMEOUT:
+			Serial.print(F("timeout"));
+			break;
+		case HTTP_ERROR_CONNECTION_FAILED:
+			Serial.print(F("Connection failed"));
+			break;
+		case HTTP_ERROR_STATUS:
+			Serial.print(F("Invalid status: "));
+			Serial.print(status);
+			switch(status){
+				case HTTP_STATUS_IDLE:
+					Serial.print(F("IDLE"));
+					break;
+				case HTTP_STATUS_RESP_WAIT:
+					Serial.print(F("RESP_WAIT"));
+					break;
+				case HTTP_STATUS_RESP_BEGIN:
+					Serial.print(F("RESP_BEGIN"));
+					break;
+				case HTTP_STATUS_RESP_HEADER:
+					Serial.print(F("RESP_HEADER"));
+					break;
+				case HTTP_STATUS_RESP_BODY:
+					Serial.print(F("RESP_BODY"));
+					break;
+				case HTTP_STATUS_ERROR:
+					Serial.print(F("ERROR"));
+					break;
+				case HTTP_STATUS_EOF:
+					Serial.print(F("EOF"));
+					break;
+				default:
+					Serial.print(status);
+			}
+			break;
+	}
+	Serial.print('\n');
+	return err;
+}
--- HttpClient.h
+++ HttpClient.h
@@ -11,36 +11,36 @@ public:
 	
 	//Les status que peut prendre la connection HTTP
 	typedef enum {
-	    HTTP_STATUS_IDLE,                //Aucune requete http en cours
-	    HTTP_STATUS_RESP_WAIT,           //En attente de la réponse
-	    HTTP_STATUS_RESP_BEGIN,          //Au début de la réponse (avant HTTP/1.1)
-	    HTTP_STATUS_RESP_HEADER,         //Au début d'une ligne d'en-tête
-	    HTTP_STATUS_RESP_HEADER_VALUE,   //Ligne d'en-tête, après le ':' (clé déjà lue)
-	    HTTP_STATUS_RESP_BODY,           //Corps de la réponse
-	    HTTP_STATUS_ERROR,               //Une erreur est survenue
-		HTTP_STATUS_EOF                  //La fin de fichier a été atteinte
+		HTTP_STATUS_IDLE,			//Aucune requete http en cours
+		HTTP_STATUS_RESP_WAIT,			//En attente de la réponse
+		HTTP_STATUS_RESP_BEGIN,			//Au début de la réponse (avant HTTP/1.1)
+		HTTP_STATUS_RESP_HEADER,		//Au début d'une ligne d'en-tête
+		HTTP_STATUS_RESP_BODY,			//Corps de la réponse
+		HTTP_STATUS_ERROR,			//Une erreur est survenue
+		HTTP_STATUS_EOF				//La fin de fichier a été atteinte
 	} HttpState;
 	
 	//Les codes retours possibles (négatifs pour les différencier du nombre de caractères que peuvent renvoyer certaines fonctions)
-	#define HTTP_EOF -1
 	#define HTTP_ERROR -2					
 	#define HTTP_ERROR_BUFFER_OVERFLOW -3
-	#define HTTP_ERROR_TIMEOUT -4;
+	#define HTTP_ERROR_TIMEOUT -4
 	#define HTTP_ERROR_CONNECTION_FAILED -5
+	#define HTTP_ERROR_STATUS -6
 	
 	#define HTTP_DEFAULT_TIMEOUT 3000;		//Timeout http par défaut
 	
-	HttpState status;                  //Status courant de la connection
+	HttpState status;				//Status courant de la connection
 	
 	void reset();
+	void close();
 	void setTimeout(int millis);
 	void setDebug(boolean debug);
 	
-	int sendGet(char* server, int port, char* path);
+	//int sendGet(char* server, int port, char* path);
+	int sendGet(char* server, int port, char* path, char* params);
 	
 	int readResponseCode();
-	int readNextHeaderKey(char* key,int maxlen);
-	int readNextHeaderValue(char* value,int maxlen);
+	int readLineToChar(char sep, char* key,int maxlen);
 	int skipHeaders();
 	int moveToEOL();
 	int available();
@@ -53,11 +53,13 @@ protected:
 	int print(char* str);
 	int println(char* str);
 	boolean connect(char* serv, int port);
+	int error(int err);
 	
 private:
-	int startMillis;                  //timestamp de démarrage de la requete en cours (sert pour le timeout)
-	int timeout;                       //configuration de timeout en cours
+	int startMillis;				//timestamp de démarrage de la requete en cours (sert pour le timeout)
+	int timeout;					//configuration de timeout en cours
 	boolean debug;
+	boolean lastConnected;
 	
 };
 
